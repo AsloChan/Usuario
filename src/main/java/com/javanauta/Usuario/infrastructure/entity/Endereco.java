1a3d64b85b963211afcaac5fa4a05cf1848c71fd
Lucas <lucas2012icm@gmail.com>
2025-11-10 21:53:05 -0300

    adicionadas classes de security

---
+++ src/main/java/com/javanauta/Usuario/infrastructure/entity/Endereco.java
@@ -0,0 +1,37 @@
+package com.javanauta.Usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table( name = "endereco")
+
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column (name = "rua")
+    private String rua;
+    @Column (name = "numero")
+    private Long numero;
+    @Column (name = "complemento", length = 10)
+    private String complemento;
+    @Column (name = "cidade", length = 150)
+    private String cidade;
+    @Column (name = "estado", length = 2)
+    private String estado;
+    @Column (name = "cep", length = 10)
+    private String cep;
+
+}
